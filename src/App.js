290275a550ed7cb5e4fcf00542fc64bed114d833
Muntaeyeon <monty420420@naver.com>
2023-03-27 21:59:57 +0900

    state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  let post = '강남 우동 맛집';
+  // document.querySelector('h4').innerHTML = post;
+  //1
+  // let[글제목1, 글제목a] = useState('남자코트 추천');
+  // let[글제목2,글제목b] = useState('여자코트 추천');
+  // let[글제목3,글제목c] = useState('어린이코트 추천');
+  //2
+  let[글제목, 글제목이다] = useState([ '남자코트 추천','여자코트 추천','어린이 코트 추천' ]);
+  
+  let [따봉, 따봉변경]= useState(0);
+  //console.log(따봉);
+
+  function 함수(){
+    // console.log(1);
+
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='black-nav'>
+             <h4 style={ {color : 'red', fontsize : '16px'} }>
+              blog
+            </h4>
+      </div>
+
+      <div className='list'>
+      <h4>{글제목[0]} <span onClick={() => 따봉변경(따봉+1) }>👍</span> {따봉} </h4>
+      <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+      <h4>{글제목[1]}</h4>
+      <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+      <h4>{글제목[2]}</h4>
+      <p>2월 17일 발행</p>
+      </div>
+      
     </div>
   );
 }
