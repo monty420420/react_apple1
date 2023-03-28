88ee46f1b1acee4f607e042d5824048dad61897d
Muntaeyeon <monty420420@naver.com>
2023-03-28 23:50:30 +0900

    array_spread

--- src/App.js
+++ src/App.js
@@ -11,18 +11,14 @@ function App() {
   // let[글제목2,글제목b] = useState('여자코트 추천');
   // let[글제목3,글제목c] = useState('어린이코트 추천');
   //2
-  let[글제목, 글제목이다] = useState([ '남자코트 추천','여자코트 추천','어린이 코트 추천' ]);
+  let[글제목, 글제목이다] = useState([ '남자코트 추천','여자코트 추천','가어린이 코트 추천' ]);
   
   let [따봉, 따봉변경]= useState(0);
   //console.log(따봉);
 
   // function 함수(){
   //   // console.log(1);
-
   // }
-
-
-
   return (
     <div className="App">
       <div className='black-nav'>
@@ -31,6 +27,24 @@ function App() {
             </h4>
       </div>
 
+      <button onClick={()=> {
+        let copy = [...글제목];
+        copy[0] = '노인코트 추천';
+        
+        let copy2 = [...글제목];
+        copy[1] = '할머니 코트';
+
+        글제목이다(copy);
+      }}>글수정</button>
+
+      <button onClick={() => {
+        let change = [...글제목];
+        change.sort();
+        글제목이다(change);
+      }}>
+        수정버튼
+      </button>
+         
       <div className='list'>
       <h4>{글제목[0]} <span onClick={() => 따봉변경(따봉+1) }>👍</span> {따봉} </h4>
       <p>2월 17일 발행</p>
