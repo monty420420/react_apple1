560c53dbccdf2ee640ab31b7a9a4f3a256d32d8d
Muntaeyeon <monty420420@naver.com>
2023-04-03 22:40:18 +0900

    글 추가 삭제

--- src/App.js
+++ src/App.js
@@ -11,11 +11,12 @@ function App() {
   // let[글제목2,글제목b] = useState('여자코트 추천');
   // let[글제목3,글제목c] = useState('어린이코트 추천');
   //2
-  let[글제목, 글제목변경] = useState([ '남자코트 추천','여자코트 추천','가어린이 코트 추천' ]);
+  let[글제목, 글제목변경] = useState(['남자코트 추천','여자코트 추천','가어린이 코트 추천' ]);
   
   let [따봉, 따봉변경]= useState([0,0,0]);
   let [openModal, setOpenModal] = useState(true);
   let [title, setTitle] = useState(0);
+  let [입력값, 입력값변경] = useState("");
 
   
 
@@ -52,29 +53,53 @@ function App() {
         글제목변경(change);
       }}>
         수정버튼
-      </button>         
+      </button>        
       
       {
          글제목.map(function(a,i){
           return(
             <div className='list' key={i}>  
                 <h4 className='list_title' onClick={()=>{{setOpenModal(false)}; setTitle(i) }}
-                >{글제목[i]} <span onClick={() => {
+                >{글제목[i]} 
+                <span onClick={(e) => {
+                  e.stopPropagation();  //이벤트 버블링 방지
                   let 따봉카피 = [...따봉]; //따봉카피라는 변수를 만들어 따봉을 깊은복사한다
                   따봉카피[i] = 따봉카피[i] +1; //버튼이 클릭되었을때 따봉카피배열에 1을 더한다
                   따봉변경(따봉카피); //set에 변경된 따봉카피를저장한다
                 } }>👍</span> {따봉[i]}</h4>
                 <p>2월 17일 발행</p>
+                <button onClick={()=>{
+                  let delCopy = [...글제목]; //글제목배열 깊은복사
+                  delCopy.splice(i,1); //splice(수정할 배열 요소의 인덱스, 삭제할 요소 개수, 추가될 요소)
+                  글제목변경(delCopy); //변경내역 저장
+                }
+                }
+                >삭제</button>
             </div>
           )
          })
       }
 
-      <button onClick={()=>{ setTitle(0)}}>글제목0</button>
+      {/* <button onClick={()=>{ setTitle(0)}}>글제목0</button>
       <button onClick={()=>{ setTitle(1)}}>글제목1</button>
       <button onClick={()=>{ setTitle(2)}}>글제목2</button>
       
-      <button onClick={()=>setOpenModal(!openModal)}>모달</button>
+      <button onClick={()=>setOpenModal(!openModal)}>모달</button> */}
+
+      <input onChange={(e)=> { 
+        // console.log(e.target.value) 
+        입력값변경(e.target.value);  //input에 value를 입력값변경에 저장
+        console.log(입력값);
+        }} />
+
+      <button onClick={()=>{
+        let addCopy = [...글제목];  //배열깊은 복사
+        addCopy.push(입력값);       //배열에 입력값 추가
+        글제목변경(addCopy);         //저장
+      }}>
+        추가버튼
+      </button>    
+        
 
       {
         openModal == 0 ? <Modal title변경={title} 글제목변경props={글제목변경} 글제목props={글제목} color="yellow"/> : null/*1. openmodal이 0이면 보여주기 아니면 null처리
